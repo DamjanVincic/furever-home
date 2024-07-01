02d03254f47d1992f160a6c0dce85be30d6b03f1
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:08:36 +0200

    Add Comment

---
+++ FureverHome/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FureverHome.Models
+{
+    public class Comment
+    {
+        public string Text {  get; set; }
+        public int UserId {  get; set; }
+        public User User { get; set; }
+
+    }
+}
