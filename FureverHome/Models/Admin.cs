84ded68af7498cc5bb330a9b719c51e0f529f076
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:00:36 +0200

    Add Admin

---
+++ FureverHome/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FureverHome.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public int AccountId {  get; set; }
+        public Account Account {  get; set; }
+    }
+}
