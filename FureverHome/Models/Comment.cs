9afd90b981d12789b7c88d3dedab28068d5b0d00
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:10:28 +0200

    Add Donation and Comment

--- FureverHome/Models/Comment.cs
+++ FureverHome/Models/Comment.cs
@@ -8,6 +8,7 @@ namespace FureverHome.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
         public string Text {  get; set; }
         public int UserId {  get; set; }
         public User User { get; set; }
--- FureverHome/Models/Donation.cs
+++ FureverHome/Models/Donation.cs
@@ -9,6 +9,7 @@ namespace FureverHome.Models
     public class Donation
     {
         public Donation() { }
+        public int Id { get; set; }
         public double Amount {  get; set; }
         public bool Anonymous {  get; set; }
         public string User { get; set; }
