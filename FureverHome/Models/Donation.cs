8b15c45cafce69a0363b2c6112591b0f6e51f085
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:09:38 +0200

    Add Donation

---
+++ FureverHome/Models/Donation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FureverHome.Models
+{
+    public class Donation
+    {
+        public Donation() { }
+        public double Amount {  get; set; }
+        public bool Anonymous {  get; set; }
+        public string User { get; set; }
+        public string Description { get; set; }
+    }
+}
