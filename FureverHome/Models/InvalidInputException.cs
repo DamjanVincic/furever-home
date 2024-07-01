d142e888925105187ae04d4293aa4f1891645cce
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:49:44 +0200

    Add InvalidExceptionInput

---
+++ FureverHome/Models/InvalidInputException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FureverHome.Models
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+        {
+        }
+
+        public InvalidInputException(string message) : base(message)
+        {
+        }
+    }
+}
