21a04f6c73464d925731996ec15a1a9ceef1ef5f
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 14:45:04 +0200

    Add styles

--- FureverHome/App.xaml.cs
+++ FureverHome/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace FureverHome;
@@ -9,4 +12,16 @@ namespace FureverHome;
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+
 }
