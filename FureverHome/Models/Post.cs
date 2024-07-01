7f87b028004a30df968205bcf09ab5e0c9494658
milicaradicc <milica.t.radic@gmail.com>
2024-07-01 15:17:02 +0200

    Add Post

---
+++ FureverHome/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FureverHome.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageLink { get; set; }
+        public DateTime Date { get; set; }
+        public PostStatus Status {  get; set; }
+        public bool IsApproved {  get; set; }
+        public List<AdoptionRequest> AdoptionRequests { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<User> Users { get; set; }
+        public int AuthorId {  get; set; }
+        public User Author {  get; set; }
+        public int AnimalId {  get; set; }
+        public Animal Animal {  get; set; }
+    }
+}
