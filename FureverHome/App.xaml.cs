2991d20f08fc7bc4eb83ec9ae2fa6a03ced5fd30
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 14:57:51 +0200

    Add repositories to DI

--- FureverHome/App.xaml.cs
+++ FureverHome/App.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
 using FureverHome.Models;
+using FureverHome.Repositories;
+using FureverHome.Repositories.PostgresRepositories;
+using FureverHome.Views;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ServiceProvider = FureverHome.Models.ServiceProvider;
 
 namespace FureverHome;
 
@@ -16,13 +20,15 @@ public partial class App : Application
 
         ServiceCollection services = new ServiceCollection();
         ConfigureServices(services);
+        
+        ServiceProvider.Instance = services.BuildServiceProvider();
     }
 
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
 
-        var mainWindow = new MainWindow();
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
         mainWindow.Show();
     }
 
@@ -30,5 +36,20 @@ public partial class App : Application
     {
         string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new InvalidInputException("Connection string not found in .env file.");
         services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
+
+        services.AddScoped<MainWindow>();
+
+        services.AddScoped<IAccountRepository, AccountPostgresRepository>();
+        services.AddScoped<IAdoptionRequestRepository, AdoptionRequestPostgresRepository>();
+        services.AddScoped<IAnimalBreedRepository, AnimalBreedPostgresRepository>();
+        services.AddScoped<IAnimalRepository, AnimalPostgresRepository>();
+        services.AddScoped<IAnimalReviewRepository, AnimalReviewPostgresRepository>();
+        services.AddScoped<IAnimalSpeciesRepository, AnimalSpeciesPostgresRepository>();
+        services.AddScoped<IAssociationRepository, AssociationPostgresRepository>();
+        services.AddScoped<IColorRepository, ColorPostgresRepository>();
+        services.AddScoped<ICommentRepository, CommentPostgresRepository>();
+        services.AddScoped<IMessageRepository, MessagePostgresRepository>();
+        services.AddScoped<IPostRepository, PostPostgresRepository>();
+        services.AddScoped<IUserRepository, UserPostgresRepository>();
     }
 }
