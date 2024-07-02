ff1a6075759ed60218a83c03a3d631ff22b372c9
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 12:13:48 +0200

    Add database context

---
+++ FureverHome/Models/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FureverHome.Models;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies();
+        if (optionsBuilder.IsConfigured) return;
+        
+        DotNetEnv.Env.Load("../.env"); // Works if the current directory is the LangLang project
+        string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new InvalidInputException("Connection string not found in .env file.");
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
