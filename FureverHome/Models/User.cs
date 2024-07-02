0172beb0066172637e378e85395886783e8680a5
zoricnadja <zoric.n2003@gmail.com>
2024-07-02 19:03:57 +0200

    Update models

--- FureverHome/Models/User.cs
+++ FureverHome/Models/User.cs
@@ -6,15 +6,13 @@
         {
         }
 
-        public User(string firstName, string lastName, Gender gender, string phone, string adress, Account account)
+        public User(string firstName, string lastName, Gender gender, string phone, string adress)
         {
             FirstName = firstName;
             LastName = lastName;
             Gender = gender;
             Phone = phone;
             Adress = adress;
-            Account = account;
-            AccountId = account.Id;
         }
 
         public int Id { get; set; }
