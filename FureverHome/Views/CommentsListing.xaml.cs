c7d6c551a75973db3c900be3a34607829959dbaf
milicaradicc <milica.t.radic@gmail.com>
2024-07-02 20:04:51 +0200

    Update views

--- FureverHome/Views/CommentsListing.xaml.cs
+++ FureverHome/Views/CommentsListing.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FureverHome.ViewModels;
+﻿using FureverHome.Services;
+using FureverHome.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System;
+using System.Linq;
+using System.Windows.Media;
+using GalaSoft.MvvmLight;
+using FureverHome.Models;
 
 namespace FureverHome.Views
 {
@@ -20,10 +26,11 @@ namespace FureverHome.Views
     /// </summary>
     public partial class CommentsListing : Window
     {
-        public CommentsListing()
+        private readonly ICommentService _commentService = ServiceProvider.GetRequiredService<ICommentService>();
+        public CommentsListing(int postId)
         {
             InitializeComponent();
-            DataContext = new CommentListingViewModel();
+            DataContext = new CommentListingViewModel(_commentService,postId);
         }
     }
 }
