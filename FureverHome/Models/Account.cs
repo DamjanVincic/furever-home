25d358b51efb605eb1c58eff0aa222e44424b08c
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 15:58:54 +0200

    Move user status to account status

--- FureverHome/Models/Account.cs
+++ FureverHome/Models/Account.cs
@@ -9,6 +9,9 @@
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public int RegisteredUserId { get; set; }
+        public virtual RegisteredUser RegisteredUser { get; set; }
         public AccountType Type { get; set; }
+        public AccountStatus Status { get; set; }
     }
 }
