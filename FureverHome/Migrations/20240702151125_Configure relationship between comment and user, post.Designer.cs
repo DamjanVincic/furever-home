8dfef73f8a5f27263d2f1f9ab667897054091056
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 17:14:49 +0200

    Add migration

--- FureverHome/Migrations/20240702151125_Configure relationship between comment and user, post.Designer.cs
+++ FureverHome/Migrations/20240702151125_Configure relationship between comment and user, post.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FureverHome.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FureverHome.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240702151125_Configure relationship between comment and user, post")]
+    partial class Configurerelationshipbetweencommentanduserpost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
