e3d2cc37b90244d653004c22ecb6ab71012f1b8c
milicaradicc <milica.t.radic@gmail.com>
2024-07-02 18:22:51 +0200

    Add CommentService

---
+++ FureverHome/Services/CommentService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FureverHome.Models;
+using FureverHome.Repositories;
+
+namespace FureverHome.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentService(ICommentRepository commentRepository) {
+            _commentRepository = commentRepository;
+        }
+        public List<Comment> GetAll()
+        {
+            return _commentRepository.GetAll();
+        }
+    }
+}
---
+++ FureverHome/Services/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FureverHome.Models;
+
+namespace FureverHome.Services
+{
+    public interface ICommentService
+    {
+       public List<Comment> GetAll();
+    }
+}
