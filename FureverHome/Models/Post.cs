2889dd482319c683c3ba6375425bde3b0e6273dd
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 13:43:49 +0200

    Initialize navigation collections in models

--- FureverHome/Models/Post.cs
+++ FureverHome/Models/Post.cs
@@ -12,9 +12,9 @@
         public DateTime Date { get; set; }
         public PostStatus Status { get; set; }
         public bool IsApproved { get; set; }
-        public virtual List<AdoptionRequest> AdoptionRequests { get; set; }
-        public virtual List<Comment> Comments { get; set; }
-        public virtual List<User> LikedBy { get; set; }
+        public virtual List<AdoptionRequest> AdoptionRequests { get; set; } = [];
+        public virtual List<Comment> Comments { get; set; } = [];
+        public virtual List<User> LikedBy { get; set; } = [];
         public int AuthorId { get; set; }
         public virtual User Author { get; set; }
         public int AnimalId { get; set; }
