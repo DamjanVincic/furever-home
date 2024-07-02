286c7a3097131d56cadc15bae8e493dee799eba1
DamjanVincic <damjanvincic2003@gmail.com>
2024-07-02 17:32:15 +0200

    Add migration

--- FureverHome/Migrations/20240702152925_Configure db relationship between message and registered user.Designer.cs
+++ FureverHome/Migrations/20240702152925_Configure db relationship between message and registered user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FureverHome.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FureverHome.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240702152925_Configure db relationship between message and registered user")]
+    partial class Configuredbrelationshipbetweenmessageandregistereduser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
