406f0ef7f6494294c4c3183ea02b85fa30dbea9e
zoricnadja <zoric.n2003@gmail.com>
2024-07-02 16:43:23 +0200

    Update user service

--- FureverHome/Services/UserService.cs
+++ FureverHome/Services/UserService.cs
@@ -1,13 +1,9 @@
 ﻿using FureverHome.Models;
 using FureverHome.Repositories;
-using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace FureverHome.Services
 {
-    internal class UserService
+    public class UserService
     {
         public static User? LoggedInUser { get; private set; }
         private readonly IUserRepository _userRepository;
@@ -30,12 +26,12 @@ namespace FureverHome.Services
         }
 
         public void Add(string? firstName, string? lastName, string? username, string? password, Gender gender, string? phone,
-            string? adress, AccountType accountType)
+            string? adress)
         {
             if (_accountRepository.GetAll().Any(account => account.UserName.Equals(username)))
                 throw new InvalidInputException("Username already exists");
 
-            Account account = new(username!, password!, accountType);
+            Account account = new(username!, password!, AccountType.User);
             int accountId = _accountRepository.Add(account);
             account.Id = accountId;
             _userRepository.Add(new User(firstName!, lastName!, gender, phone!, adress!, account));
@@ -71,10 +67,14 @@ namespace FureverHome.Services
                 LoggedInUser = null;
         }
 
-        public User? Login(string email, string password)
+        public User? Login(string username, string password)
         {
             User? user = _userRepository.GetAll()
-                .FirstOrDefault(user => user.Account.UserName.Equals(email) && user.Account.Password.Equals(password));
+                .FirstOrDefault(user => user.Account.UserName.Equals(username) && user.Account.Password.Equals(password));
+            if (user!.Account.Status.Equals(AccountStatus.Pending))
+            {
+                throw new InvalidOperationException("You are on the approval waiting list.");
+            }
             LoggedInUser = user;
             return user;
         }
