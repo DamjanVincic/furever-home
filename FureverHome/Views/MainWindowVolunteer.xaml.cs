2146c9c0297c9dcc2641a0cda813fc320a9e6ebd
milicaradicc <milica.t.radic@gmail.com>
2024-07-02 20:30:07 +0200

    Add menu bar/header

--- FureverHome/Views/MainWindowVolunteer.xaml.cs
+++ FureverHome/Views/MainWindowVolunteer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FureverHome.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace FureverHome.Views
         public MainWindowVolunteer()
         {
             InitializeComponent();
+            DataContext = new MainViewModel(this);
         }
     }
 }
